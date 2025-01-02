0731c2cfafb43ce80cc71f1fdb69d0941cdb4fc5
Osa-nag00 <naghiseosa2000@gmail.com>
2025-01-02 18:35:42 -0500

    move netrw++ plugin to custom folder

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -107,4 +107,20 @@ return {
       vim.cmd.colorscheme 'catppuccin'
     end,
   },
+
+  {
+    'prichrd/netrw.nvim',
+    opts = {
+
+      -- File icons to use when `use_devicons` is false or if
+      -- no icon is found for the given file type.
+      icons = {
+        symlink = '',
+        directory = '',
+        file = '',
+      },
+      -- Uses mini.icon or nvim-web-devicons if true, otherwise use the file icon specified above
+      use_devicons = true,
+    },
+  },
 }
