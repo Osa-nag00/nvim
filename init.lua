3c0b319d623677b9e13ef4c6193d685738118726
Osa-nag00 <naghiseosa2000@gmail.com>
2025-01-18 17:46:23 -0500

    got the dap working for python and c(sort of)

--- init.lua
+++ init.lua
@@ -943,7 +943,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   { import = 'custom.plugins' },
-  -- { import = 'debugger.plugins' }, TODO: work on setting this up at a later date
+  { import = 'debugger.plugins' }, -- TODO: work on setting this up at a later date
 
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
--- lua/debugger/plugins/init.lua
+++ lua/debugger/plugins/init.lua
@@ -1,53 +1,113 @@
 -- this custom init file is for debugger plugins and things related to that nature
 return {
 
-  -- nvim-dap debug asapter protocol
+  -- plugin for nvim dap
   {
     'mfussenegger/nvim-dap',
-    config = function() end,
+    config = function()
+      local dap = require 'dap'
+
+      vim.keymap.set('n', '<F5>', function()
+        require('dap').continue()
+      end)
+      vim.keymap.set('n', '<F6>', function()
+        require('dap').step_into()
+      end)
+      vim.keymap.set('n', '<F7>', function()
+        require('dap').step_over()
+      end)
+      vim.keymap.set('n', '<F8>', function()
+        require('dap').step_out()
+      end)
+      vim.keymap.set('n', '<M-b>', function()
+        require('dap').toggle_breakpoint()
+      end)
+
+      -- altering the styles of dap visuals
+      vim.fn.sign_define('DapBreakpoint', { text = '', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
+      vim.fn.sign_define('DapBreakpointCondition', { text = 'ﳁ', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
+      vim.fn.sign_define('DapBreakpointRejected', { text = '', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
+      vim.fn.sign_define('DapLogPoint', { text = '', texthl = 'DapLogPoint', linehl = 'DapLogPoint', numhl = 'DapLogPoint' })
+      vim.fn.sign_define('DapStopped', { text = '', texthl = 'DapStopped', linehl = 'DapStopped', numhl = 'DapStopped' })
+
+      -- start of dap configs for c/c++/rust
+      -- dap.adapters.gdb = {
+      --   type = 'executable',
+      --   command = 'gdb',
+      --   args = { '--interpreter=dap', '--eval-command', 'set print pretty on' },
+      -- }
+
+      dap.configurations.c = {
+        {
+          name = 'Launch',
+          type = 'gdb',
+          request = 'launch',
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+          cwd = '${workspaceFolder}',
+          stopAtBeginningOfMainSubprogram = false,
+        },
+      }
+      -- end of dap configs for c/c++/rust
+    end,
   },
 
+  -- plugin to add virtual text support next to src code when debugging
+  {
+    'theHamsta/nvim-dap-virtual-text',
+    config = function()
+      require('nvim-dap-virtual-text').setup()
+    end,
+  },
+
+  -- plugin for dap UI
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' },
+    config = function()
+      local dap, dapui = require 'dap', require 'dapui'
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+        vim.cmd 'NvimTreeResize 30'
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+
+        vim.cmd 'NvimTreeResize 30'
+      end
+
+      require('dapui').setup()
+    end,
+  },
+
+  -- NOTE: this also needs to be loaded after mason has been loaded
+  -- pluging mason integration with nvim dap
   {
     'jay-babu/mason-nvim-dap.nvim',
     config = function()
       require('mason-nvim-dap').setup {
         ensure_installed = {},
-        automatic_installation = true,
-        handlers = {
-          function(config)
-            -- all sources with no handler get passed here
-
-            -- Keep original functionality
-            require('mason-nvim-dap').default_setup(config)
-          end,
-        },
+        handlers = {},
+        automatic_installation = false,
       }
     end,
   },
 
-  { 'nvim-neotest/nvim-nio' },
+  -- NOTE: Below is for language specific daps
 
+  -- plugin for dap for python
   {
-    'rcarriga/nvim-dap-ui',
-    dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' },
+    'mfussenegger/nvim-dap-python',
     config = function()
-      local dap = require 'dap'
-      local dapui = require 'dapui'
-
-      dap.listeners.after['dap.event.initialize'] = function()
-        dapui.open()
-      end
-
-      dap.listeners.before['dap.event.terminated'] = function()
-        dapui.close()
-      end
-
-      -- Set up DAP UI keybindings
-      vim.api.nvim_set_keymap('n', '<F5>', ":lua require'dap'.continue()<CR>", { noremap = true })
-      vim.api.nvim_set_keymap('n', '<F10>', ":lua require'dap'.step_over()<CR>", { noremap = true })
-      vim.api.nvim_set_keymap('n', '<F11>', ":lua require'dap'.step_into()<CR>", { noremap = true })
-      vim.api.nvim_set_keymap('n', '<F12>', ":lua require'dap'.step_out()<CR>", { noremap = true })
-      vim.api.nvim_set_keymap('n', '<Leader>b', ":lua require'dap'.toggle_breakpoint()<CR>", { noremap = true })
+      require('dap-python').setup 'python3'
     end,
   },
 }
