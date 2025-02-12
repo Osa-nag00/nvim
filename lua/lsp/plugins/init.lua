542993bd5a32eef780a9011e55ffe679bc192be7
Osa-nag00 <naghiseosa2000@gmail.com>
2025-02-11 21:42:01 -0500

    added rust lsp stuff

--- lua/lsp/plugins/init.lua
+++ lua/lsp/plugins/init.lua
@@ -185,19 +185,8 @@ return {
         clangd = {
           cmd = { 'clangd' },
         },
-
-        rust_analyzer = {
-          settings = {
-            ['rust-analyzer'] = {
-              diagnostics = {
-                enable = false,
-              },
-            },
-          },
-        },
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -252,5 +241,12 @@ return {
         },
       }
     end,
+  },
+
+  -- plugin for rust handles lsp and some other stuff
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^5', -- Recommended
+    lazy = false, -- This plugin is already lazy
   },
 }
