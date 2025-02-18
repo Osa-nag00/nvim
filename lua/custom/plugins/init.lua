b514f09dc3138d009820a3c552068870e55b5700
Osa-nag00 <naghiseosa2000@gmail.com>
2025-02-17 20:41:45 -0500

    removing substitute commands I don't use

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -39,14 +39,14 @@ return {
     },
 
     -- these keymaps work with whats in the yank buffer
-    vim.keymap.set('n', 's', '', { noremap = true }),
+    -- vim.keymap.set('n', 's', '', { noremap = true }),
     vim.keymap.set('n', 'ss', "<cmd>lua require('substitute').line()<cr>", { noremap = true }),
-    vim.keymap.set('n', 'S', "<cmd>lua require('substitute').eol()<cr>", { noremap = true }),
+    -- vim.keymap.set('n', 'S', "<cmd>lua require('substitute').eol()<cr>", { noremap = true }),
 
     -- these keymaps work off of motions <motion1> <leader>[] <mation2>
-    vim.keymap.set('n', '<leader>s', "<cmd>lua require('substitute.range').operator()<cr>", { noremap = true }),
+    -- vim.keymap.set('n', '<leader>s', "<cmd>lua require('substitute.range').operator()<cr>", { noremap = true }),
     vim.keymap.set('x', '<leader>s', "<cmd>lua require('substitute.range').visual()<cr>", { noremap = true }),
-    vim.keymap.set('n', '<leader>ss', "<cmd>lua require('substitute.range').word()<cr>", { noremap = true }),
+    -- vim.keymap.set('n', '<leader>ss', "<cmd>lua require('substitute.range').word()<cr>", { noremap = true }),
   },
 
   -- catppuccin plugin for neovim themes
