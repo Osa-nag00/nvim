6539dbd468b5571510310d94a05bb4617567237d
Osa-nag00 <naghiseosa2000@gmail.com>
2025-06-03 17:56:40 -0400

    fixing noice config loading before plugin would install

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -1,5 +1,4 @@
 return {
-  -- lazy.nvim
   {
     'folke/noice.nvim',
     event = 'VeryLazy',
@@ -10,54 +9,56 @@ return {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       'MunifTanjim/nui.nvim',
     },
-  },
 
-  require('noice').setup {
-    -- lsp = {
-    --   -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-    --   override = {
-    --     ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-    --     ['vim.lsp.util.stylize_markdown'] = true,
-    --     ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
-    --   },
-    -- },
-    -- you can enable a preset for easier configuration
-    presets = {
-      long_message_to_split = true, -- long messages will be sent to a split
-      inc_rename = false, -- enables an input dialog for inc-rename.nvim
-      lsp_doc_border = false, -- add a border to hover docs and signature help
-    },
+    config = function()
+      require('noice').setup {
+        -- lsp = {
+        --   -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        --   override = {
+        --     ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        --     ['vim.lsp.util.stylize_markdown'] = true,
+        --     ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+        --   },
+        -- },
+        -- you can enable a preset for easier configuration
+        presets = {
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
 
-    -- shows popupmenu with cmd line popup center of page
-    views = {
-      cmdline_popup = {
-        position = {
-          row = '40%',
-          col = '50%',
+        -- shows popupmenu with cmd line popup center of page
+        views = {
+          cmdline_popup = {
+            position = {
+              row = '40%',
+              col = '50%',
+            },
+            size = {
+              width = 60,
+              height = 'auto',
+            },
+          },
+          popupmenu = {
+            relative = 'editor',
+            position = {
+              row = '50%',
+              col = '50%',
+            },
+            size = {
+              width = 60,
+              height = 10,
+            },
+            border = {
+              style = 'rounded',
+              padding = { 0, 1 },
+            },
+            win_options = {
+              winhighlight = { Normal = 'Normal', FloatBorder = 'DiagnosticInfo' },
+            },
+          },
         },
-        size = {
-          width = 60,
-          height = 'auto',
-        },
-      },
-      popupmenu = {
-        relative = 'editor',
-        position = {
-          row = '50%',
-          col = '50%',
-        },
-        size = {
-          width = 60,
-          height = 10,
-        },
-        border = {
-          style = 'rounded',
-          padding = { 0, 1 },
-        },
-        win_options = {
-          winhighlight = { Normal = 'Normal', FloatBorder = 'DiagnosticInfo' },
-        },
-      },
-    },
+      }
+    end,
   },
 }
