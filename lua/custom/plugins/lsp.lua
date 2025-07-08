226c0360949db6462c9de19b3792d67a34d36ae1
Osa Naghise <onaghise@deloitte.com>
2025-07-08 11:10:02 -0400

    making small edit to plugins

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -225,11 +225,9 @@ return {
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        ts_ls = {},
-        --
 
+        ts_ls = {},
+        bashls = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
