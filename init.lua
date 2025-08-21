9fcd04f60c79e841fcd6f5fc219c40289b8d4eae
Osa Naghise <onaghise@deloitte.com>
2025-08-21 15:10:45 -0400

    bugfix for when buffer close

--- init.lua
+++ init.lua
@@ -137,14 +137,6 @@ vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
   end,
 })
 
-vim.api.nvim_create_autocmd({ 'BufLeave', 'BufWinLeave' }, {
-  pattern = '*.*',
-  callback = function()
-    -- open nvim tree, only when curent buffer not active
-    vim.cmd 'NvimTreeOpen'
-  end,
-})
-
 -- Auto commands for when a git conflict is detected and when its resolved
 vim.api.nvim_create_autocmd('User', {
   pattern = 'GitConflictDetected',
