26de8c478dc3d0e4aba1b9b6102dcf9ec8af1e94
Osa Naghise <onaghise@deloitte.com>
2025-08-27 10:30:16 -0400

    moved autocmd for csv to plugin def

--- lua/custom/plugins/csv-view.lua
+++ lua/custom/plugins/csv-view.lua
@@ -3,6 +3,7 @@
 return {
   {
     'hat0uma/csvview.nvim',
+
     ---@module "csvview"
     ---@type CsvView.Options
     opts = {
@@ -17,6 +18,26 @@ return {
     },
     cmd = { 'CsvViewEnable', 'CsvViewDisable', 'CsvViewToggle' },
 
+    init = function()
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
+        pattern = '*.csv',
+        callback = function()
+          local csvView = require 'csvview'
+          local winid = vim.api.nvim_get_current_win()
+          local bufId = vim.api.nvim_get_current_buf()
+          vim.wo[winid].wrap = false
 
-  }
+          if not csvView.is_enabled(bufId) then
+            csvView.enable(bufId, {
+              view = {
+                display_mode = 'border',
+                header_lnum = 1,
+                sticky_header = { enabled = true },
+              },
+            })
+          end
+        end,
+      })
+    end,
+  },
 }
