438caeb547d223349cac73bb59c09b4719d1c19f
Osa Naghise <naghiseosa2000@gmail.com>
2026-03-08 12:42:50 -0400

    remove stuff that just doesn't get used

--- lua/plugins/screenkey.lua
+++
@@ -1,6 +0,0 @@
-return {
-  -- show keys on screen with :Screenkey
-  'NStefan002/screenkey.nvim',
-  lazy = false,
-  version = '*', -- or branch = "dev", to use the latest commit
-}
--- lua/plugins/vim-sleuth.lua
+++
@@ -1,4 +0,0 @@
-return {
-  -- /home/onaghise/.editorconfig <- config for this lives here
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-}
