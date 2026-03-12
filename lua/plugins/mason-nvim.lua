ff68e748bb80dd52eb288502b97b9cb2636eefe4
Osa Naghise <onaghise@deloitte.com>
2026-03-12 17:21:30 -0400

    some edits were made :)

--- lua/plugins/mason-nvim.lua
+++ lua/plugins/mason-nvim.lua
@@ -1,5 +1,11 @@
 return {
-  { 'mason-org/mason-lspconfig.nvim', opts = {} },
-  {"rshkarin/mason-nvim-lint", opts = {}}, 
   { 'mason-org/mason.nvim', opts = {} },
+  {
+    'mason-org/mason-lspconfig.nvim',
+    dependencies = { 'mason-org/mason.nvim' },
+    opts = {
+      ensure_installed = { 'lua_ls', 'harper_ls', 'html' },
+    },
+  },
+  { 'rshkarin/mason-nvim-lint', opts = {} },
 }
