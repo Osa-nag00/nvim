3503bdf16cdf92ae685d7c17ea38a464c4f2439a
Osa Naghise <onaghise@deloitte.com>
2026-07-29 15:35:13 -0400

    yeah more changes that make this better maybe, will prob remove in 3 months

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -31,9 +31,10 @@ return {
       -- ['<C-p>'] = { 'select_prev', 'fallback' },
       -- ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
       -- ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
-      -- ['<C-k>'] = { 'show', 'show_documentation', 'hide_documentation' },
       -- ['<C-e>'] = { 'cancel', 'fallback' },
       ['<C-y>'] = { 'select_and_accept' },
+      ['<C-Space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+      ['<C-k>'] = { 'show', 'show_documentation', 'hide_documentation' },
     },
 
     appearance = {
