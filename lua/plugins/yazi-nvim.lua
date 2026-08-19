ddfdcadcc9fc5121bad509f504c73f13de7d2782
Osa Naghise <naghiseosa2000@gmail.com>
2026-08-19 19:20:37 -0400

    yeah

--- lua/plugins/yazi-nvim.lua
+++ lua/plugins/yazi-nvim.lua
@@ -1,29 +1,18 @@
 ---@type LazySpec
 return {
-  "mikavilpas/yazi.nvim",
-  version = "*", -- use the latest stable version
-  event = "VeryLazy",
+  'mikavilpas/yazi.nvim',
+  version = '*', -- use the latest stable version
+  event = 'VeryLazy',
   dependencies = {
-    { "nvim-lua/plenary.nvim", lazy = true },
+    { 'nvim-lua/plenary.nvim', lazy = true },
   },
   keys = {
     -- 👇 in this section, choose your own keymappings!
     {
-      "<leader>o",
-      mode = { "n", "v" },
-      "<cmd>Yazi<cr>",
-      desc = "Open yazi at the current file",
-    },
-    {
-      -- Open in the current working directory
-      "<leader>cw",
-      "<cmd>Yazi cwd<cr>",
-      desc = "Open the file manager in nvim's working directory",
-    },
-    {
-      "<c-up>",
-      "<cmd>Yazi toggle<cr>",
-      desc = "Resume the last yazi session",
+      '<leader>o',
+      mode = { 'n', 'v' },
+      '<cmd>Yazi<cr>',
+      desc = 'Open yazi at the current file',
     },
   },
   ---@type YaziConfig | {}
@@ -31,7 +20,7 @@ return {
     -- if you want to open yazi instead of netrw, see below for more info
     open_for_directories = true,
     keymaps = {
-      show_help = "<f1>",
+      show_help = '<f1>',
     },
   },
   -- 👇 if you use `open_for_directories=true`, this is recommended
